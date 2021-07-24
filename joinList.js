fcfb9f7b775a335161289309d259c07a1b1bbed3
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-07-24 12:52:54 -0400

    commiting

--- joinList.js
+++ joinList.js
@@ -1,18 +1,45 @@
-const joinList = function(conceptList) {
-  let addStrings = "";
-  for (let i = 0; i < conceptList.length; i++) {
-    const word = conceptList[i];
-    console.log(i, conceptList.length);
-    if (i < conceptList.length - 1) {
-      addStrings += word.toString() + ", ";
-    } else {
-      addStrings += word.toString() + "";
-    }
-  }
-  return addStrings;
-};
+// const joinList = function(conceptList) {
+//   let addStrings = "";
+//   for (let i = 0; i < conceptList.length; i++) {
+//     const word = conceptList[i];
+//     console.log(i, conceptList.length);
+//     if (i < conceptList.length - 1) {
+//       addStrings += word.toString() + ", ";
+//     } else {
+//       addStrings += word.toString() + "";
+//     }
+//   }
+//   return addStrings;
+// };
 
-// Test / Driver Code below...
-const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
-const concepts = joinList(conceptList);
-console.log(`Today I learned about ${concepts}.`);
+// // Test / Driver Code below...
+// const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
+// const concepts = joinList(conceptList);
+// console.log(`Today I learned about ${concepts}.`);
+
+
+
+
+
+function average(list) {
+    var sum = 0;
+  
+    for (var num of list) {
+      sum += num;
+    }
+
+  
+    return sum / list.length;
+}
+  console.log(average([3, 5, 7]));
+
+
+
+  var input = process.argv.slice(2);
+
+if (input)
+    console.log(reverse(input));
+
+function reverse(original) {
+    return orignal.split('').reverseList().join('');
+}
---
+++ password.js
@@ -0,0 +1,61 @@
+
+
+// var arr=['abracadabra']
+// var str = arr.toString()
+
+// let result = '';
+
+
+// for (let letter of str) {
+//   if (letter === "a"){
+//       letter=4
+//    }
+
+//    else if (letter === "e"){
+//     letter=3
+//  }
+
+//  else if (letter === "o"){
+//     letter=0
+//  }
+
+//  else if (letter === "l"){
+//     letter=1
+//  }
+
+
+//   result += letter
+// }
+// console.log(result)
+
+
+
+let arr=process.argv.slice(2)
+let str=arr.toString()
+function obfuscate (str){
+
+let result= ""
+for (let letter of str) {
+    if (letter === "a"){
+        letter=4
+     }
+  
+     else if (letter === "e"){
+      letter=3
+   }
+  
+   else if (letter === "o"){
+      letter=0
+   }
+  
+   else if (letter === "l"){
+      letter=1
+   }
+   else {result= str}
+  
+    
+  }
+  return result  += letter
+}
+
+obfuscate(str)
