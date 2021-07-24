352d4522a82ee622de689cea043db43a59036ff1
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-07-24 13:40:51 -0400

    correting password.js

--- password.js
+++ password.js
@@ -29,6 +29,39 @@
 // console.log(result)
 
 
+//  wrong answer:
+
+// let arr=process.argv.slice(2)
+// let str=arr.toString()
+// function obfuscate (str){
+
+// let result= ""
+// for (let letter of str) {
+//     if (letter === "a"){
+//         letter=4
+//      }
+  
+//      else if (letter === "e"){
+//       letter=3
+//    }
+  
+//    else if (letter === "o"){
+//       letter=0
+//    }
+  
+//    else if (letter === "l"){
+//       letter=1
+//    }
+//    else {result= str}
+  
+    
+//   }
+//   return result  += letter
+// }
+
+// obfuscate(str)
+
+
 
 let arr=process.argv.slice(2)
 let str=arr.toString()
@@ -51,11 +84,12 @@ for (let letter of str) {
    else if (letter === "l"){
       letter=1
    }
-   else {result= str}
+    {result+= letter}
   
     
   }
-  return result  += letter
+  console.log(result)
+  return result  
 }
 
 obfuscate(str)
